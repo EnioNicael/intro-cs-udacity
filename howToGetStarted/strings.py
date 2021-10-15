36d18667e5956a194e7bf5687bd35795c82bd4b2
Enio Nicael <enionicael@gmail.com>
2021-10-15 01:49:52 -0300

    Indexing strings

--- howToGetStarted/strings.py
+++ howToGetStarted/strings.py
@@ -3,4 +3,14 @@
 name = 'Yoda'
 
 print(f'Hello {name}') # Hello Yoda
-print('!' * 5) # !!!!! 
+print('!' * 5) # !!!!!
+
+
+# Indexing Strings
+# <string>[<expression>]
+
+print('udacity'[0]) # u
+print('udacity'[1 + 1]) # a
+name = 'Yoda'
+print(name[0]) # Y
+
