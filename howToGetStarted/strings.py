d61b80bd86fec8dcfdeac6addc398f334d871e21
Enio Nicael <enionicael@gmail.com>
2021-10-17 01:52:20 -0300

    Selecting sub sequences

--- howToGetStarted/strings.py
+++ howToGetStarted/strings.py
@@ -14,3 +14,16 @@ print('udacity'[1 + 1]) # a
 name = 'Yoda'
 print(name[0]) # Y
 
+
+# Selecting sub-sequences
+#
+#            start           stop
+# <string>[<expression> : <expression>]]
+#            number          number
+
+word = 'udacity'
+print(word[3:3]) # ""
+print(word[:3]) # uda
+print(word[3:]) # city
+print(word[2:6]) # acit
+print(word[:]) # udacity
